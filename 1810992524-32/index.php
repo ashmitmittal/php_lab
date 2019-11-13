0c69e6676d7e03db75ad2543eb8631b1c82cd962
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-11-13 18:38:05 +0530

    final

--- 1810992524-32/index.php
+++ 1810992524-32/index.php
@@ -8,7 +8,15 @@
 </head>
 <body>
 <h1>Database connection</h1>
+<form action="" method="POST">
+    <input type="text" name="name">
+    <button type="submit" name="submit">Submit</button>
+</form>
+<form method="POST">
+    <button type="submit" name="show">Show data</button>
+</form>
     <?php
+    if(isset($_POST['show'])){
         include_once 'includes/dbs.php';
         $query = "SELECT * FROM SAMPLE;";
         $result=mysqli_query($con,$query);
@@ -16,6 +24,15 @@
         {
             echo $row['name']."<br>";
         }
+    }
+    if(isset($_POST['submit'])){
+        $name = $_POST['name'];
+        include_once 'includes/dbs.php';
+        $query = "INSERT INTO SAMPLE(name) VALUE('$name');";
+        if(!mysqli_query($con,$query)){
+            echo 'error';
+        }
+    }
     ?>
 </body>
 </html>
