5fc6ded9ccc3e3915ae1263edc387b098b4106ba
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-11-13 17:13:54 +0530

    update

--- ST-2/3/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$text = '<p>My name is <b><i>Ashmit</i></b></p>';
-echo "String with html tags :- ".$text;
-
-echo "Now without Tags :- ".strip_tags($text);
--- testing.php
+++
@@ -1,3 +0,0 @@
-<?php
-$str = "hello world i am ashmit";
-print_r (explode(' ',$str));
