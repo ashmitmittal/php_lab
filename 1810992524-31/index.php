61abb039d29941b5adbf85f0898fd84b68c660fd
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-11-27 22:34:21 +0530

    c

--- 1810992524-31/index.php
+++ 1810992524-31/index.php
@@ -11,16 +11,16 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <title>Contact Form</title>
 </head>
-<body>
-    <main class="container jumbotron">
-        <p>SEND E-MAIL</p>
-        <form class="" action="contact.php" method="POST">
-            <input type="text" name="name" placeholder="Full Name">
-            <input type="text" name="mail" placeholder="Your e-mail">
-            <input type="text" name="subject" placeholder="Subject">
-            <textarea name="message" placeholder="Message"></textarea>
-            <button type="submit" name="submit">SEND MAIL</button>
-        </form>
-    </main>
-</body>
+    <body>
+        <main class="container jumbotron">
+            <p>SEND E-MAIL</p>
+            <form class="" action="contact.php" method="POST">
+                <input type="text" name="name" placeholder="Full Name">
+                <input type="text" name="mail" placeholder="Your e-mail">
+                <input type="text" name="subject" placeholder="Subject">
+                <textarea name="message" placeholder="Message"></textarea>
+                <button type="submit" name="submit">SEND MAIL</button>
+            </form>
+        </main>
+    </body>
 </html>
